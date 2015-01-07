4bfe915531bbedca736543e38018efc0e0f2f030
Kevin Paul <kpaul@ucar.edu>
2015-01-07 20:51:52 +0000

    Cleaned up the unittest output when run in parallel by capturing the output into a "string stream" (Python StringIO object) that I then message to the master rank.  I then output each test's output in sequence with header info about to which rank the results pertain.

--- test/messengerParallelTests.py
+++ test/messengerParallelTests.py
@@ -8,41 +8,62 @@ Created on Jan 7, 2015
 '''
 import unittest
 import messenger
+from mpi4py import MPI
 
 
 class MessengerParallelTests(unittest.TestCase):
     '''
     Parallel Messenger unit tests.
     '''
-
     def test_init(self):
         msngr = messenger.MPIMessenger()
         self.assertIsInstance(msngr, messenger.Messenger,
                               'Failed to create class instance')
-        self.assertEqual(msngr._mpi_rank, msngr.get_rank(),
+        self.assertEqual(msngr._mpi_rank, MPI.COMM_WORLD.Get_rank(),
                          'Rank is wrong after initialization')
-        self.assertEqual(msngr._mpi_size, msngr.get_size(),
+        self.assertEqual(msngr._mpi_size, MPI.COMM_WORLD.Get_size(),
                          'Size is wrong after initialization')
-        self.assertEqual(msngr._is_master, (0 == msngr.get_rank()),
+        self.assertEqual(msngr._is_master, (0 == MPI.COMM_WORLD.Get_rank()),
                          'Is_master is wrong after initialization')
+        self.assertEqual(msngr._mpi_comm, MPI.COMM_WORLD,
+                         'MPI Comm is wrong after initialization')
         self.assertEqual(msngr.verbosity, 1,
                          'Verbosity is wrong after initialization')
 
-    def test_partition(self):
+    def test_get_rank(self):
+        msngr = messenger.MPIMessenger()
+        self.assertEqual(msngr.get_rank(), MPI.COMM_WORLD.Get_rank(),
+                        'Parallel messenger rank wrong')
+
+    def test_get_size(self):
+        msngr = messenger.MPIMessenger()
+        self.assertEqual(msngr.get_size(), MPI.COMM_WORLD.Get_size(),
+                        'Parallel messenger size wrong')
+
+    def test_is_master(self):
+        msngr = messenger.MPIMessenger()
+        if MPI.COMM_WORLD.Get_rank() == 0:
+            self.assertTrue(msngr.is_master(),
+                            'Parallel messenger should be master')
+        else:
+            self.assertFalse(msngr.is_master(),
+                            'Parallel messenger should not be master')
+
+    def test_partition_unweighted(self):
         msngr = messenger.MPIMessenger()
         data = [i for i in range(msngr.get_size())]
         p_data = msngr.partition(data)
         self.assertListEqual(p_data, [msngr.get_rank()],
                          'Parallel partition is wrong')
 
-    def test_is_master(self):
+    def test_partition_weighted(self):
         msngr = messenger.MPIMessenger()
-        if msngr.get_rank() == 0:
-            self.assertTrue(msngr.is_master(),
-                            'Parallel messenger should be master')
-        else:
-            self.assertFalse(msngr.is_master(),
-                            'Parallel messenger should not be master')
+        rank = msngr.get_rank()
+        size = msngr.get_size()
+        data = {i:(size - i) for i in range(size)}
+        p_data = msngr.partition(data)
+        self.assertListEqual(p_data, [size - rank - 1],
+                         'Parallel partition is wrong')
 
     def test_sum_list(self):
         msngr = messenger.MPIMessenger()
@@ -89,5 +110,21 @@ class MessengerParallelTests(unittest.TestCase):
         msngr.prinfo(msg, vlevel=0, master=False)
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    hline = '=' * 70
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        print hline
+        print 'STANDARD OUTPUT FROM ALL TESTS:'
+        print hline
+
+    from cStringIO import StringIO
+    mystream = StringIO()
+    tests = unittest.TestLoader().loadTestsFromTestCase(MessengerParallelTests)
+    unittest.TextTestRunner(stream=mystream).run(tests)
+
+    results = MPI.COMM_WORLD.gather(mystream.getvalue())
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        for rank, result in enumerate(results):
+            print hline
+            print 'TESTS RESULTS FOR RANK ' + str(rank) + ':'
+            print hline
+            print str(result)
