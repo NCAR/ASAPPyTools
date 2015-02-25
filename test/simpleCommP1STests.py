889f8a0cf80b823e8a441cce76d3a0bc8de09955
Kevin Paul <kpaul@ucar.edu>
2015-02-25 00:41:48 +0000

    Adding new 1-rank parallel vs serial tests.

--- test/simpleCommP1STests.py
+++ test/simpleCommP1STests.py
@@ -14,7 +14,7 @@ Created on Feb 17, 2015
 import unittest
 import simplecomm
 import numpy as np
-from partfunc import equal_stride
+from partitioning import EqualStride
 from os import linesep
 from mpi4py import MPI
 MPI_COMM_WORLD = MPI.COMM_WORLD
@@ -50,136 +50,125 @@ class SimpleCommP1STests(unittest.TestCase):
         print msg
         self.assertEqual(sresult, presult, msg)
 
-    def testIsMaster(self):
-        sresult = self.scomm.is_master()
-        presult = self.pcomm.is_master()
-        msg = test_info_msg('is_master()', None, sresult, presult)
+    def testIsManager(self):
+        sresult = self.scomm.is_manager()
+        presult = self.pcomm.is_manager()
+        msg = test_info_msg('is_manager()', None, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testSumInt(self):
         data = 5
-        sresult = self.scomm.reduce(data)
-        presult = self.pcomm.reduce(data)
+        sresult = self.scomm.allreduce(data, 'sum')
+        presult = self.pcomm.allreduce(data, 'sum')
         msg = test_info_msg('sum(int)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testSumList(self):
         data = range(5)
-        sresult = self.scomm.reduce(data)
-        presult = self.pcomm.reduce(data)
+        sresult = self.scomm.allreduce(data, 'sum')
+        presult = self.pcomm.allreduce(data, 'sum')
         msg = test_info_msg('sum(list)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testSumDict(self):
         data = {'rank': self.rank, 'range': range(3 + self.rank)}
-        sresult = self.scomm.reduce(data)
-        presult = self.pcomm.reduce(data)
+        sresult = self.scomm.allreduce(data, 'sum')
+        presult = self.pcomm.allreduce(data, 'sum')
         msg = test_info_msg('sum(list)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testSumArray(self):
         data = np.arange(5)
-        sresult = self.scomm.reduce(data)
-        presult = self.pcomm.reduce(data)
+        sresult = self.scomm.allreduce(data, 'sum')
+        presult = self.pcomm.allreduce(data, 'sum')
         msg = test_info_msg('sum(array)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testMaxInt(self):
         data = 13 + self.rank
-        sresult = self.scomm.reduce(data, op=max)
-        presult = self.pcomm.reduce(data, op=max)
+        sresult = self.scomm.allreduce(data, 'max')
+        presult = self.pcomm.allreduce(data, 'max')
         msg = test_info_msg('max(int)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testMaxList(self):
         data = range(5 + self.rank)
-        sresult = self.scomm.reduce(data, op=max)
-        presult = self.pcomm.reduce(data, op=max)
+        sresult = self.scomm.allreduce(data, 'max')
+        presult = self.pcomm.allreduce(data, 'max')
         msg = test_info_msg('max(list)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testMaxDict(self):
         data = {'rank': self.rank, 'range': range(3 + self.rank)}
-        sresult = self.scomm.reduce(data, op=max)
-        presult = self.pcomm.reduce(data, op=max)
+        sresult = self.scomm.allreduce(data, 'max')
+        presult = self.pcomm.allreduce(data, 'max')
         msg = test_info_msg('max(dict)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
     def testMaxArray(self):
         data = np.arange(5 + self.rank)
-        sresult = self.scomm.reduce(data, op=max)
-        presult = self.pcomm.reduce(data, op=max)
+        sresult = self.scomm.allreduce(data, 'max')
+        presult = self.pcomm.allreduce(data, 'max')
         msg = test_info_msg('max(array)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
-    def testGatherInt(self):
-        if self.pcomm.is_master():
-            data = None
-            sresult = self.scomm.gather()
-            presult = self.pcomm.gather()
-        else:
-            data = 10
-            sresult = self.scomm.gather(data)
-            presult = self.pcomm.gather(data)
-        msg = test_info_msg('Gather(int)', data, sresult, presult)
+    def testPartitionInt(self):
+        data = 13 + self.rank
+        sresult = self.scomm.partition(data)
+        presult = self.pcomm.partition(data)
+        msg = test_info_msg('partition(int)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
-    def testGatherList(self):
-        data = range(3 + self.rank)
-        sresult = self.scomm.gather(data)
-        presult = self.pcomm.gather(data)
-        msg = test_info_msg('Gather(list)', data, sresult, presult)
+    def testPartitionIntInvolved(self):
+        data = 13 + self.rank
+        sresult = self.scomm.partition(data, involved=True)
+        presult = self.pcomm.partition(data, involved=True)
+        msg = test_info_msg('partition(int, T)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
-    def testScatterInt(self):
-        if self.pcomm.is_master():
-            data = 100
-            sresult = self.scomm.scatter(data)
-            presult = self.pcomm.scatter(data)
-        else:
-            data = None
-            sresult = self.scomm.scatter()
-            presult = self.pcomm.scatter()
-        msg = test_info_msg('Scatter(int)', data, sresult, presult)
+    def testPartitionList(self):
+        data = range(5 + self.rank)
+        sresult = self.scomm.partition(data, func=EqualStride())
+        presult = self.pcomm.partition(data, func=EqualStride())
+        msg = test_info_msg('partition(list)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
-    def testScatterList(self):
-        if self.pcomm.is_master():
-            data = range(10)
-            sresult = self.scomm.scatter(data, part=equal_stride)
-            presult = self.pcomm.scatter(data, part=equal_stride)
-        else:
-            data = None
-            sresult = self.scomm.scatter()
-            presult = self.pcomm.scatter()
-        msg = test_info_msg('Scatter(list)', data, sresult, presult)
+    def testPartitionListInvolved(self):
+        data = range(5 + self.rank)
+        sresult = self.scomm.partition(data, func=EqualStride(), involved=True)
+        presult = self.pcomm.partition(data, func=EqualStride(), involved=True)
+        msg = test_info_msg('partition(list, T)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
 
-    def testScatterListSkip(self):
-        if self.pcomm.is_master():
-            data = range(10)
-            sresult = self.scomm.scatter(data, part=equal_stride, skip=True)
-            presult = self.pcomm.scatter(data, part=equal_stride, skip=True)
-        else:
-            data = None
-            sresult = self.scomm.scatter()
-            presult = self.pcomm.scatter()
-        msg = test_info_msg('Scatter(list, skip)', data, sresult, presult)
+    def testPartitionArray(self):
+        data = np.arange(2 + self.rank)
+        sresult = self.scomm.partition(data)
+        presult = self.pcomm.partition(data)
+        msg = test_info_msg('partition(array)', data, sresult, presult)
         print msg
         self.assertEqual(sresult, presult, msg)
+
+    def testPartitionArrayInvolved(self):
+        data = np.arange(2 + self.rank)
+        sresult = self.scomm.partition(data, involved=True)
+        presult = self.pcomm.partition(data, involved=True)
+        msg = test_info_msg('partition(array, T)', data, sresult, presult)
+        print msg
+        np.testing.assert_array_equal(sresult, presult, msg)
+
 
 
 if __name__ == "__main__":
