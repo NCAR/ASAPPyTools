80d349cd2b0ba34e7ce82143133f3011ca057637
Kevin Paul <kpaul@ucar.edu>
2015-03-31 22:13:03 +0000

    Better long description in the setup script.

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from distutils.core import setup
 
 setup(name='ASAPTools',
       version='0.3',
-      description='A collection of useful Python modules',
+      description='A collection of useful Python modules from the '
+                  'Application Scalability And Performance (ASAP) group '
+                  'at the National Center for Atmospheric Research',
       author='Kevin Paul',
       author_email='kpaul@ucar.edu',
       packages=['asaptools'],
