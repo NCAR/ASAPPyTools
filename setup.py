380783a3fb3f7869ba340bc4dbe8d03d2b779f35
Kevin Paul <kpaul@ucar.edu>
2015-06-10 16:58:06 -0600

    Updating version number

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='ASAPTools',
-      version='0.4',
+      version='0.4.1',
       description='A collection of useful Python modules from the '
                   'Application Scalability And Performance (ASAP) group '
                   'at the National Center for Atmospheric Research',
       author='Kevin Paul',
       author_email='kpaul@ucar.edu',
       url='https://github.com/NCAR-CISL-ASAP/ASAPPyTools',
-      download_url='https://github.com/NCAR-CISL-ASAP/ASAPPyTools/tarball/v0.4',
+      download_url='https://github.com/NCAR-CISL-ASAP/ASAPPyTools/tarball/v0.4.1',
       license='https://github.com/NCAR-CISL-ASAP/ASAPPyTools/blob/master/LICENSE.txt',
       packages=['asaptools'],
       package_dir={'asaptools': 'source/asaptools'},
