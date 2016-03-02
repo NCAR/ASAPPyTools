8f715a86ee23995e39fb5adfc18a9894caa2d918
Kevin Paul <kpaul@ucar.edu>
2016-03-02 14:37:13 -0700

    Updating copyright dates in tests.

--- source/asaptools/test/vprinterTests.py
+++ source/asaptools/test/vprinterTests.py
@@ -1,7 +1,8 @@
 """
-Created on Feb 26, 2015
+Tests of the verbose printer utility
 
-@author: Kevin Paul <kpaul@ucar.edu>
+Copyright 2016, University Corporation for Atmospheric Research
+See the LICENSE.txt file for details
 """
 import unittest
 import sys
