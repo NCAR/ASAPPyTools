0b1466653c768de066c136c33a7d176e04c59f5b
Kevin Paul <kpaul@ucar.edu>
2018-12-10 16:17:17 -0700

    Ignoring egg-info

---
+++ ci/install-travis.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+echo "[install-travis]"
+
+MINICONDA_DIR="$HOME/miniconda3"
+time wget -q http://repo.continuum.io/miniconda3/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || exit 1
+time bash miniconda.sh -b -p "$MINICONDA_DIR" || exit 1
+
+echo
+echo "[show conda]"
+which conda
+
+echo
+echo "[update conda]"
+conda config --set always_yes true --set changeps1 false || exit 1
+conda config --add channels conda-forge
+conda update -q conda
+
+echo
+echo "[install dependencies]"
+conda create -q -n asaptools-dev python=${PYTHON}
+conda env update -f environment-dev.yml
+source activate asaptools-dev
+conda list
+
+echo
+echo "[install asaptools]"
+pip install --no-deps -e .
+
+echo "[finished install]"
+
+exit 0
