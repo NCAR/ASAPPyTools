717967cfb4350d3582b426d994ab6890f17c3872
Kevin Paul <kpaul@ucar.edu>
2018-12-10 16:36:38 -0700

    Reinstall versioneer

--- asaptools/_version.py
+++ asaptools/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pip440"
+    cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "asaptools-"
     cfg.versionfile_source = "asaptools/_version.py"
