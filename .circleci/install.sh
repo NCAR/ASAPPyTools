a65ead66498016517e89448c33133c9f0ff3c7dd
Kevin Paul <kpaul@ucar.edu>
2019-01-30 07:56:02 -0700

    Need source activate, not conda activate

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -9,7 +9,7 @@ conda config --set pip_interop_enabled True # Enable pip interoperability
 conda config --add channels conda-forge
 conda env create -f .circleci/env-${PYTHON}-${MPI}.yml --name=${ENV_NAME} --quiet
 conda env list
-conda activate ${ENV_NAME}
+source activate ${ENV_NAME}
 pip install pip --upgrade
 pip install --no-deps --quiet -e .
 conda list -n ${ENV_NAME}
